ea0ec5607a26b8de7dad3c19882a0113094ba019
neozenweb <37545009+neozenweb@users.noreply.github.com>
2018-10-12 07:45:17 -0400

    Add files via upload

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,11 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-         it('URL defined and not empty',function(){
+       it('URL defined and not empty',function(){
              allFeeds.forEach(function(feed){
                  expect(feed.url).toBeDefined();
                  expect(feed.url.length).not.toBe(0);
@@ -39,10 +35,7 @@ $(function() {
              
             });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        
         
          it('name defined and not empty',function(){
              allFeeds.forEach(function(feed){
@@ -59,19 +52,10 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-
     
      describe('The Menu', function() {
         
     
-    
-    
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
 
          it('Menu is hidden by default',function(){
              
@@ -79,36 +63,20 @@ $(function() {
              
              
          });
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-           it('Toggle Menu',function(){
+     
+         it('Toggle Menu',function(){
              
-                   $('body.menu-icon-link').trigger('click');
-                           
-                  expect($('body.menu-hidden').css('visibility')).toBe('visible');
+               $('.menu-icon-link').trigger('click');
+                expect($('body.menu-hidden').css('visibility')).not.toBe('visible');
+               $('.menu-icon-link').trigger('click');
+                expect($('body.menu-hidden').css('visibility')).toBe('visible');
              
          });
        
          
     });   
-    /* TODO: Write a new test suite named "Initial Entries" */
+ 
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
-
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
 
      describe('Initial Entries', function(){
           beforeEach(function(done){
@@ -124,19 +92,21 @@ $(function() {
      });
 
      describe('New Feed Selection', function(){
-         
+         var feed1="";
+         var feed2 ="";
           beforeEach(function(done){
-                  
-                  loadFeed(0);
-                  loadFeed(1);
-                 loadFeed(2,done);
-           
-              
+                   
+                   loadFeed(0,function(){
+                   feed1 = $('.feed .entry')[0].innerText;
+                   loadFeed(1,function(){
+                   feed2 = $('.feed .entry')[0].innerText;
+                   done();
+                          });
+                    });
+               
               });
          it('content actually changes',function(){
-                const fd1 = $('.feed')[0].children[0].innerText;
-                const fd2 = $('.feed')[0].children[1].innerText;
-                expect(fd1 === fd2).toBe(false);
+                  expect(feed1 === feed2).toBe(false);
              
             
          });
